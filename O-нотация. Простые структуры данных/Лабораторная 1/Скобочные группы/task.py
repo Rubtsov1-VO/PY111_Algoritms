750c26db24215421ad22bf8bc18d44dc407d57d4
vladimirrubtsov <Vladimir08!>
2025-06-11 09:18:51 +0300

    lab 1 is done

--- O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
+++ O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
@@ -6,7 +6,15 @@ def check_brackets(brackets_row: str) -> bool:
     :return: True, если последовательность корректна, False в противном случае
     """
     ...  # TODO реализовать проверку скобочной группы
-
+    stack = []
+    for char in brackets_row:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return len(stack) == 0
 
 if __name__ == '__main__':
     print(check_brackets("()()"))  # True
